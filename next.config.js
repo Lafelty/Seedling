a63dd2e8f2705e51fdcc76d91e73fa008cab1d2a
Lafelty <pittithada.punpun@gmail.com>
2026-06-27 23:01:01 +0700

    Fix Vercel build: ES modules, Tailwind border, TensorFlow transpile

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['@tensorflow/tfjs', '@tensorflow-models/pose-detection'],
+}
 
 module.exports = nextConfig

e5be8076eb55eeaa3ebf895a93b5b79b2493811c
Lafelty <pittithada.punpun@gmail.com>
2026-06-27 23:15:45 +0700

    Fix: Revert to CommonJS for Next.js 16 + Tailwind v3 compatibility

---
+++ .next/server/chunks/ssr/_next-internal_server_app_session_page_actions_1n3jo2u.js
@@ -0,0 +1,3 @@
+module.exports=[57670,a=>{"use strict";a.s([])}];
+
+//# sourceMappingURL=_next-internal_server_app_session_page_actions_1n3jo2u.js.map
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@tensorflow/tfjs', '@tensorflow-models/pose-detection'],
+  serverExternalPackages: ['@mediapipe/pose'],
 }
 
 module.exports = nextConfig
